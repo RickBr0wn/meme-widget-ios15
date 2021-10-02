e3ac341df0d69e23f53c5b38bb28f1047c35481d
Rick Brown <ricky.brown.00@gmail.com>
2021-10-02 22:31:16 +0100

    Initial Commit

---
+++ meme-widget-ios15/meme_widget_ios15App.swift
@@ -0,0 +1,17 @@
+//
+//  meme_widget_ios15App.swift
+//  meme-widget-ios15
+//
+//  Created by Rick Brown on 02/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct meme_widget_ios15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
